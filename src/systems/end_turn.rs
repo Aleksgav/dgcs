5b0f2c57b26c9a0b276198628ec6e8b47c0e53db
Gavrishchuk Aleksandr <alex.phyp@gmail.com>
2022-08-04 04:58:33 +0300

    [feat]: added stairs between levels

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -11,11 +11,11 @@ pub fn end_turn(
 ) {
     let mut player_hp = <(&Health, &Point)>::query().filter(component::<Player>());
     let mut amulet = <&Point>::query().filter(component::<AmuletOfYala>());
-
+    let amulet_default = Point::new(-1, -1);
     let amulet_pos = amulet
         .iter(ecs)
         .nth(0)
-        .unwrap();
+        .unwrap_or(&amulet_default);
 
     let current_state = turn_state.clone();
     let mut new_state = match current_state {
